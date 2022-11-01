fd20799d32a5851b4a407ae07738fdaf2de13ca6
KrzywdzinskiGrzegorz <krzywdzinskigrzegorz@gmail.com>
2022-11-01 23:28:04 +0100

    Add TheEnd method

--- EvenNumber/Program.cs
+++ EvenNumber/Program.cs
@@ -10,6 +10,51 @@ namespace EvenNumber
     {
         static void Main(string[] args)
         {
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Podaj liczbę:");
+                    if (int.TryParse(Console.ReadLine(), out var number))
+                    {
+                        if (number % 2 == 0)
+                            Console.WriteLine($"Liczba {number} jest parzysta.");
+                        else
+                            Console.WriteLine($"Liczba {number} jest nieparzysta.");
+                    }
+                    else
+                        Console.WriteLine("Wprowadziłeś nieprawidłowe dane !!!");
+                } while (TheEnd());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+           
         }
+
+        static bool TheEnd()
+        {
+            try
+            {
+                string end;
+                Console.WriteLine("Czy chcesz zakończyć, odpowiedz T lub N:");
+
+                do
+                {
+                    end = Console.ReadLine().ToUpper();
+                } while (!(end.Equals("N") || end.Equals("T")));
+
+                if (end.Equals("N")) return true;
+                else return false;
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return true;
+            }
+        } 
     }
 }
